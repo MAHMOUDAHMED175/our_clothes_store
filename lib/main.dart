ef5435e3cc8edcf6029dcdc6ccb01cab09bf1dc1
MAHMOUDAHMED175 <eng.mahmoudahmed176@gmail.com>
2024-03-30 16:58:37 +0200

    device oriantation

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,28 @@ import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'our_clothes.dart';
 
 void main() async {
-  
   WidgetsFlutterBinding.ensureInitialized();
 
   Platform.isAndroid
       ? await Firebase.initializeApp(
           options: const FirebaseOptions(
-              apiKey: "AIzaSyDJTcSw1DP3hTb4isCH4sim1DFiCn1HvJM",
-              appId: "1:1003026458129:android:4849282b4144214a8b73b0",
-              messagingSenderId: "1003026458129",
-              projectId: "ourclothes-fcdb6"))
+            apiKey: 'AIzaSyDJTcSw1DP3hTb4isCH4sim1DFiCn1HvJM',
+            appId: '1:1003026458129:android:4849282b4144214a8b73b0',
+            messagingSenderId: '1003026458129',
+            projectId: 'ourclothes-fcdb6',
+          ),
+        )
       : await Firebase.initializeApp();
 
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+
   runApp(
-    OurClothes()
+    const OurClothes(),
   );
 }
