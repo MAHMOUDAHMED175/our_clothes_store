37b593c7463ebaa5d828ba8296fad3e7f621d3fb
MAHMOUDAHMED175 <eng.mahmoudahmed176@gmail.com>
2024-04-01 11:35:00 +0200

    font_family

---
+++ lib/core/style/fonts/font_family_helper.dart
@@ -0,0 +1,17 @@
+
+class FontFamilyHelper {
+  const FontFamilyHelper._();
+
+  static const String cairoArabic = 'Cairo';
+
+  static const String poppinsEnglish = 'Poppins';
+
+  // static String geLocalozedFontFamily() {
+  //   final currentLanguage = SharedPref().getString(PrefKeys.language);
+  //   if (currentLanguage == 'ar') {
+  //     return cairoArabic;
+  //   } else {
+  //     return poppinsEnglish;
+  //   }
+  // }
+}
--- lib/our_clothes.dart
+++ lib/our_clothes.dart
@@ -3,6 +3,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:our_clothes_store/core/app/connectivity_controller.dart';
 import 'package:our_clothes_store/core/app/env.variable.dart';
 import 'package:our_clothes_store/core/common/screen/no_network_screen.dart';
+import 'package:our_clothes_store/core/style/fonts/font_family_helper.dart';
+import 'package:our_clothes_store/core/style/fonts/font_weight_helper.dart';
 
 class OurClothes extends StatelessWidget {
   const OurClothes({super.key});
@@ -22,8 +24,32 @@ class OurClothes extends StatelessWidget {
                 colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
                 useMaterial3: true,
               ),
-              home: Scaffold(
-                appBar: AppBar(title: const Icon(Icons.reddit)),
+              home: const Scaffold(
+                body: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'YourMAHMOUD ahmed ??',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: FontFamilyHelper.poppinsEnglish,
+                          fontWeight: FontWeightHelper.bold,
+                        ),
+                      ),
+                      Text(
+                        'هل انت محمود احمد ??',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: FontFamilyHelper.cairoArabic,
+                          fontWeight: FontWeightHelper.medium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
               builder: (context, child) {
                 return Scaffold(
