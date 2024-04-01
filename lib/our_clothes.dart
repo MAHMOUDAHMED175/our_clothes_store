2c07b73b870802aa3a12e8dc3b1bfd98730ba3e6
MAHMOUDAHMED175 <eng.mahmoudahmed176@gmail.com>
2024-04-01 11:05:41 +0200

    native_splash_screen_and_screen_util

--- lib/our_clothes.dart
+++ lib/our_clothes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:our_clothes_store/core/app/connectivity_controller.dart';
 import 'package:our_clothes_store/core/app/env.variable.dart';
 import 'package:our_clothes_store/core/common/screen/no_network_screen.dart';
@@ -12,26 +13,29 @@ class OurClothes extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (context, value, child) {
         if (value) {
-          return MaterialApp(
-            title: 'OurClothes',
-            debugShowCheckedModeBanner: EnvVaiable.instance.debugmod,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
+          return ScreenUtilInit(
+            designSize: const Size(375, 812),
+            child: MaterialApp(
+              title: 'OurClothes',
+              debugShowCheckedModeBanner: EnvVaiable.instance.debugmod,
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              home: Scaffold(
+                appBar: AppBar(title: const Icon(Icons.reddit)),
+              ),
+              builder: (context, child) {
+                return Scaffold(
+                  body: Builder(
+                    builder: (context) {
+                      ConnectivityController.instance.init();
+                      return child!;
+                    },
+                  ),
+                );
+              },
             ),
-            home: Scaffold(
-              appBar: AppBar(title: const Icon(Icons.reddit)),
-            ),
-            builder: (context, child) {
-              return Scaffold(
-                body: Builder(
-                  builder: (context) {
-                    ConnectivityController.instance.init();
-                    return child!;
-                  },
-                ),
-              );
-            },
           );
         } else {
           return MaterialApp(
