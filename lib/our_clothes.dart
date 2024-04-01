926082c69057a777a1a4f3b2c0f82a05f31ddefd
MAHMOUDAHMED175 <eng.mahmoudahmed176@gmail.com>
2024-04-01 10:06:17 +0200

    connectivity

--- lib/our_clothes.dart
+++ lib/our_clothes.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:our_clothes_store/core/app/connectivity_controller.dart';
 import 'package:our_clothes_store/core/app/env.variable.dart';
+import 'package:our_clothes_store/core/common/screen/no_network_screen.dart';
 
 class OurClothes extends StatelessWidget {
   const OurClothes({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'OurClothes',
-      debugShowCheckedModeBanner: EnvVaiable.instance.debugmod,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(appBar: AppBar(title: const Icon(Icons.reddit)),),
+    return ValueListenableBuilder(
+      valueListenable: ConnectivityController.instance.isConnected,
+      builder: (context, value, child) {
+        if (value) {
+          return MaterialApp(
+            title: 'OurClothes',
+            debugShowCheckedModeBanner: EnvVaiable.instance.debugmod,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: Scaffold(
+              appBar: AppBar(title: const Icon(Icons.reddit)),
+            ),
+            builder: (context, child) {
+              return Scaffold(
+                body: Builder(
+                  builder: (context) {
+                    ConnectivityController.instance.init();
+                    return child!;
+                  },
+                ),
+              );
+            },
+          );
+        } else {
+          return MaterialApp(
+            title: 'No Netwrok',
+            debugShowCheckedModeBanner: EnvVaiable.instance.debugmod,
+            home: const NoNetWorkScreen(),
+          );
+        }
+      },
     );
   }
-
- 
- }
+}
